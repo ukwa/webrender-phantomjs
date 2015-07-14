4331e85a8582cf51b7288400a3abb0a26a0370d7
Roger G. Coram <roger.coram@bl.uk>
2015-07-14 15:59:58 +0100

    Amended example to output JSON via 'fs'.

--- examples/netsniff-rasterize.js
+++ examples/netsniff-rasterize.js
@@ -10,237 +10,239 @@
  */
 
 if (!Date.prototype.toISOString) {
-	Date.prototype.toISOString = function () {
-		function pad(n) { return n < 10 ? '0' + n : n; }
-		function ms(n) { return n < 10 ? '00'+ n : n < 100 ? '0' + n : n }
-		return this.getFullYear() + '-' +
-			pad(this.getMonth() + 1) + '-' +
-			pad(this.getDate()) + 'T' +
-			pad(this.getHours()) + ':' +
-			pad(this.getMinutes()) + ':' +
-			pad(this.getSeconds()) + '.' +
-			ms(this.getMilliseconds()) + 'Z';
-	}
+    Date.prototype.toISOString = function () {
+        function pad(n) { return n < 10 ? '0' + n : n; }
+        function ms(n) { return n < 10 ? '00'+ n : n < 100 ? '0' + n : n }
+        return this.getFullYear() + '-' +
+            pad(this.getMonth() + 1) + '-' +
+            pad(this.getDate()) + 'T' +
+            pad(this.getHours()) + ':' +
+            pad(this.getMinutes()) + ':' +
+            pad(this.getSeconds()) + '.' +
+            ms(this.getMilliseconds()) + 'Z';
+    }
 }
 
 capture = function(clipRect) {
-	if (clipRect) {
-		 if (!typeof(clipRect) === "object") {
-			throw new Error("clipRect must be an Object instance.");
-		}
-	}
-	try {
-		page.clipRect = clipRect;
-		return page.renderBase64("PNG");
-	} catch (e) {
-		console.log("Failed to capture screenshot: " + e, "error");
-	}
+    if (clipRect) {
+         if (!typeof(clipRect) === "object") {
+            throw new Error("clipRect must be an Object instance.");
+        }
+    }
+    try {
+        page.clipRect = clipRect;
+        return page.renderBase64("PNG");
+    } catch (e) {
+        console.log("Failed to capture screenshot: " + e, "error");
+    }
 }
 
 captureSelector = function(selector) {
-	var clipRect = page.evaluate(function(selector) {
-		var e = document.querySelector(selector);
-		if(e != null) {
-			return document.querySelector(selector).getBoundingClientRect();
-		}
-	}, selector);
-	if(clipRect != null) {
-		return capture(clipRect);
-	}
+    var clipRect = page.evaluate(function(selector) {
+        var e = document.querySelector(selector);
+        if(e != null) {
+            return document.querySelector(selector).getBoundingClientRect();
+        }
+    }, selector);
+    if(clipRect != null) {
+        return capture(clipRect);
+    }
 }
 
 function createHAR(address, title, startTime, resources, b64_content, selectors, clickables)
 {
-	var entries = [];
-	resources.forEach(function (resource) {
-		var request = resource.request,
-			startReply = resource.startReply,
-			endReply = resource.endReply;
+    var entries = [];
+    resources.forEach(function (resource) {
+        var request = resource.request,
+            startReply = resource.startReply,
+            endReply = resource.endReply;
 
-		if (!request || !startReply || !endReply) {
-			return;
-		}
+        if (!request || !startReply || !endReply) {
+            return;
+        }
 
-		// Exclude Data URI from HAR file because
-		// they aren't included in specification
-		if (request.url.match(/(^data:image\/.*)/i)) {
-			//Include these; we're already breaking the spec.
-			//return;
-		}
+        // Exclude Data URI from HAR file because
+        // they aren't included in specification
+        if (request.url.match(/(^data:image\/.*)/i)) {
+            //Include these; we're already breaking the spec.
+            //return;
+        }
 
-		entries.push({
-			startedDateTime: request.time.toISOString(),
-			time: endReply.time - request.time,
-			request: {
-				method: request.method,
-				url: request.url,
-				httpVersion: "HTTP/1.1",
-				cookies: [],
-				headers: request.headers,
-				queryString: [],
-				headersSize: -1,
-				bodySize: -1
-			},
-			response: {
-				status: endReply.status,
-				statusText: endReply.statusText,
-				httpVersion: "HTTP/1.1",
-				cookies: [],
-				headers: endReply.headers,
-				redirectURL: "",
-				headersSize: -1,
-				bodySize: startReply.bodySize,
-				content: {
-					size: startReply.bodySize,
-					mimeType: endReply.contentType
-				}
-			},
-			cache: {},
-			timings: {
-				blocked: 0,
-				dns: -1,
-				connect: -1,
-				send: 0,
-				wait: startReply.time - request.time,
-				receive: endReply.time - startReply.time,
-				ssl: -1
-			},
-			pageref: address
-		});
-	});
+        entries.push({
+            startedDateTime: request.time.toISOString(),
+            time: endReply.time - request.time,
+            request: {
+                method: request.method,
+                url: request.url,
+                httpVersion: "HTTP/1.1",
+                cookies: [],
+                headers: request.headers,
+                queryString: [],
+                headersSize: -1,
+                bodySize: -1
+            },
+            response: {
+                status: endReply.status,
+                statusText: endReply.statusText,
+                httpVersion: "HTTP/1.1",
+                cookies: [],
+                headers: endReply.headers,
+                redirectURL: "",
+                headersSize: -1,
+                bodySize: startReply.bodySize,
+                content: {
+                    size: startReply.bodySize,
+                    mimeType: endReply.contentType
+                }
+            },
+            cache: {},
+            timings: {
+                blocked: 0,
+                dns: -1,
+                connect: -1,
+                send: 0,
+                wait: startReply.time - request.time,
+                receive: endReply.time - startReply.time,
+                ssl: -1
+            },
+            pageref: address
+        });
+    });
 
-	var renderedElements = [];
-	selectors.forEach(function(selector) {
-		var image = captureSelector(selector);
-		if(image != null) {
-			renderedElements.push({
-				selector: selector,
-				format: "PNG",
-				content: image,
-				encoding: "base64"
-			});
-		}
-	});
+    var renderedElements = [];
+    selectors.forEach(function(selector) {
+        var image = captureSelector(selector);
+        if(image != null) {
+            renderedElements.push({
+                selector: selector,
+                format: "PNG",
+                content: image,
+                encoding: "base64"
+            });
+        }
+    });
 
-	return {
-		log: {
-			version: '0.0.2',
-			creator: {
-				name: "PhantomJS",
-				version: phantom.version.major + '.' + phantom.version.minor +
-					'.' + phantom.version.patch
-			},
-			pages: [{
-				startedDateTime: startTime.toISOString(),
-				id: address,
-				title: title,
-				pageTimings: {
-					onLoad: page.endTime - page.startTime
-				},
-				renderedContent: {
-					text: b64_content,
-					encoding: "base64"
-				},
-				renderedElements: renderedElements,
-				map: clickables
-			}],
-			entries: entries
-		}
-	};
+    return {
+        log: {
+            version: '0.0.2',
+            creator: {
+                name: "PhantomJS",
+                version: phantom.version.major + '.' + phantom.version.minor +
+                    '.' + phantom.version.patch
+            },
+            pages: [{
+                startedDateTime: startTime.toISOString(),
+                id: address,
+                title: title,
+                pageTimings: {
+                    onLoad: page.endTime - page.startTime
+                },
+                renderedContent: {
+                    text: b64_content,
+                    encoding: "base64"
+                },
+                renderedElements: renderedElements,
+                map: clickables
+            }],
+            entries: entries
+        }
+    };
 }
 
 var doRender = function () {
-	page.endTime = new Date();
-	page.title = page.evaluate(function () {
-		return document.title;
-	});
-	var clickables = page.evaluate(function() {
-		var clickables = [];
-		var elements = Array.prototype.slice.call(document.getElementsByTagName("*"));
-		elements.forEach(function(element) {
-			if(element.offsetParent != null) {
-				if(element.onclick != null || element.attributes["href"] != undefined) {
-					var c = {};
-					c.location = element.getBoundingClientRect();
-					if(element.attributes["href"] != undefined) {
-						c.href = element.attributes["href"].textContent;
-					}
-					if(element.onclick != null) {
-						c.onclick = element.onclick.toString();
-					}
-					clickables.push(c);
-				}
-			}
-		});
-		return clickables;
-	});
-	var selectors = phantom.args.slice(1);
-	var b64_content = window.btoa(unescape(encodeURIComponent(page.content)));
-	var har = createHAR(page.address, page.title, page.startTime, page.resources, b64_content, selectors, clickables);
-	console.log(JSON.stringify(har, undefined, 4));
-	phantom.exit();
+    page.endTime = new Date();
+    page.title = page.evaluate(function () {
+        return document.title;
+    });
+    var clickables = page.evaluate(function() {
+        var clickables = [];
+        var elements = Array.prototype.slice.call(document.getElementsByTagName("*"));
+        elements.forEach(function(element) {
+            if(element.offsetParent != null) {
+                if(element.onclick != null || element.attributes["href"] != undefined) {
+                    var c = {};
+                    c.location = element.getBoundingClientRect();
+                    if(element.attributes["href"] != undefined) {
+                        c.href = element.attributes["href"].textContent;
+                    }
+                    if(element.onclick != null) {
+                        c.onclick = element.onclick.toString();
+                    }
+                    clickables.push(c);
+                }
+            }
+        });
+        return clickables;
+    });
+    var output = phantom.args[2];
+    var selectors = phantom.args.slice(2);
+    var b64_content = window.btoa(unescape(encodeURIComponent(page.content)));
+    var har = createHAR(page.address, page.title, page.startTime, page.resources, b64_content, selectors, clickables);
+    fs.write(output, JSON.stringify(har, undefined, 4), "w");
+    phantom.exit();
 };
 
 var page = require('webpage').create(),
-	system = require('system'),
-	count = 0,
-	resourceWait  = 500, 
-	maxRenderWait = 30000,
-	forcedRenderTimeout,
-	renderTimeout;
+    system = require('system'),
+    count = 0,
+    resourceWait  = 500, 
+    maxRenderWait = 30000,
+    forcedRenderTimeout,
+    renderTimeout;
+    var fs = require("fs");
 
 if (system.args.length === 1) {
-	console.log('Usage: netsniff-rasterize.js URL selectors...');
-	phantom.exit(1);
+    console.log('Usage: netsniff-rasterize.js URL output-file selectors...');
+    phantom.exit(1);
 } else {
-	page.address = system.args[1];
-	page.resources = [];
+    page.address = system.args[1];
+    page.resources = [];
 
-	page.onLoadStarted = function () {
-		page.startTime = new Date();
-	};
+    page.onLoadStarted = function () {
+        page.startTime = new Date();
+    };
 
-	page.onResourceRequested = function (req) {
-		count += 1;
-		page.resources[req.id] = {
-			request: req,
-			startReply: null,
-			endReply: null
-		};
-		clearTimeout(renderTimeout);
-	};
+    page.onResourceRequested = function (req) {
+        count += 1;
+        page.resources[req.id] = {
+            request: req,
+            startReply: null,
+            endReply: null
+        };
+        clearTimeout(renderTimeout);
+    };
 
-	page.onResourceReceived = function (res) {
-		if (res.stage === 'start') {
-			page.resources[res.id].startReply = res;
-		}
-		if (res.stage === 'end') {
-			page.resources[res.id].endReply = res;
-		}
-		 if(!res.stage || res.stage === "end") {
-			count -= 1;
-			page.viewportSize = {
-				width: 1280,
-				height: page.evaluate(function() {
-					return document.body.scrollHeight;
-				})
-			};
-			if(count === 0) {
-				renderTimeout = setTimeout(doRender, resourceWait);
-			}
-		}
-	};
+    page.onResourceReceived = function (res) {
+        if (res.stage === 'start') {
+            page.resources[res.id].startReply = res;
+        }
+        if (res.stage === 'end') {
+            page.resources[res.id].endReply = res;
+        }
+         if(!res.stage || res.stage === "end") {
+            count -= 1;
+            page.viewportSize = {
+                width: 1280,
+                height: page.evaluate(function() {
+                    return document.body.scrollHeight;
+                })
+            };
+            if(count === 0) {
+                renderTimeout = setTimeout(doRender, resourceWait);
+            }
+        }
+    };
 
-	page.viewportSize = { width: 1280, height: 960 };
+    page.viewportSize = { width: 1280, height: 960 };
 
-	page.open(page.address, function (status) {
-		if (status !== 'success') {
-			console.log('FAIL to load the address');
-			phantom.exit(1);
-		} else {
-			forcedRenderTimeout = setTimeout(function () {
-				doRender();
-			}, maxRenderWait);
-		}
-	});
+    page.open(page.address, function (status) {
+        if (status !== 'success') {
+            console.log('FAIL to load the address');
+            phantom.exit(1);
+        } else {
+            forcedRenderTimeout = setTimeout(function () {
+                doRender();
+            }, maxRenderWait);
+        }
+    });
 }
--- phantomjs/views.py
+++ phantomjs/views.py
@@ -13,85 +13,89 @@ from django.views.decorators.gzip import gzip_page
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpResponseServerError
 
-logger = logging.getLogger( "phantomjs.views" )
+logger = logging.getLogger("phantomjs.views")
 
-def generate_image( url ):
+def generate_image(url):
     """Returns a 1280x960 rendering of the webpage."""
-    tmp = temp + str( random.randint( 0, 100 ) ) + ".jpg"
-    image = Popen( [ phantomjs, rasterize, url, tmp ], stdout=PIPE, stderr=PIPE )
+    tmp = "%s/%s.jpg" % (temp, str(random.randint(0, 100)))
+    image = Popen([phantomjs, rasterize, url, tmp], stdout=PIPE, stderr=PIPE)
     stdout, stderr = image.communicate()
-    im = Image.open( tmp )
-    crop = im.crop( ( 0, 0, 1280, 960 ) )
-    crop.save( tmp, "JPEG" )
-    data = open( tmp, "rb" ).read()
-    os.remove( tmp )
+    im = Image.open(tmp)
+    crop = im.crop((0, 0, 1280, 960))
+    crop.save(tmp, "JPEG")
+    data = open(tmp, "rb").read()
+    os.remove(tmp)
     return data
 
-def get_image( request, url ):
+def get_image(request, url):
     """Tries to render an image of a URL, returning a 500 if it times out."""
-    data = generate_image( url )
-    return HttpResponse( content=data, content_type="image/jpeg" )
+    data = generate_image(url)
+    return HttpResponse(content=data, content_type="image/jpeg")
 
-def strip_debug( js ):
+def strip_debug(js):
     """PhantomJs seems to merge its output with its error messages; this
     tries to strip them."""
     lines = js.decode("utf-8").splitlines()
-    for index, line in enumerate( lines ):
-        if line.startswith( "{" ):
-            return "\n".join( lines[ index: ] )
+    for index, line in enumerate(lines):
+        if line.startswith("{"):
+            return "\n".join(lines[index:])
     return js
 
-def get_har( url ):
+def get_har(url):
     """Gets the raw HAR output from PhantomJs."""
-    har = Popen( [ phantomjs, netsniff, url ], stdout=PIPE, stderr=PIPE )
+    har = Popen([phantomjs, netsniff, url], stdout=PIPE, stderr=PIPE)
     stdout, stderr = har.communicate()
-    return strip_debug( stdout )
+    return strip_debug(stdout)
 
-def get_har_with_image( url, selectors=None ):
+def get_har_with_image(url, selectors=None):
     """Gets the raw HAR output from PhantomJs with rendered image(s)."""
-    command = [ phantomjs, domimage, url ]
+    tmp = "%s/%s.json" % (temp, str(random.randint(0, 100)))
+    command = [phantomjs, domimage, url, tmp]
     if selectors is not None:
         command += selectors
-    har = Popen( command, stdout=PIPE, stderr=PIPE )
+    har = Popen(command, stdout=PIPE, stderr=PIPE)
     stdout, stderr = har.communicate()
-    return strip_debug( stdout )
+    with open(tmp, "rb") as i:
+        output = i.read()
+    os.remove(tmp)
+    return output
 
-def get_raw( request, url ):
+def get_raw(request, url):
     """Tries to retrieve the HAR, returning a 500 if timing out."""
-    js = get_har( url )
-    return HttpResponse( content=js, content_type="application/json" )
+    js = get_har(url)
+    return HttpResponse(content=js, content_type="application/json")
 
-def generate_urls( url ):
+def generate_urls(url):
     """Tries to retrieve a list of URLs from the HAR."""
-    js = get_har( url )
+    js = get_har(url)
 
-    data = json.loads( js )
-    return "\n".join( [ entry[ "request" ][ "url" ] for entry in data[ "log" ][ "entries" ] ] )
+    data = json.loads(js)
+    return "\n".join([entry["request"]["url"] for entry in data["log"]["entries"]])
 
-def get_urls( request, url ):
+def get_urls(request, url):
     """Tries to retrieve a list of URLs, returning a 500 if timing out."""
-    response = generate_urls( url )
-    return HttpResponse( content=response, content_type="text/plain" )
+    response = generate_urls(url)
+    return HttpResponse(content=response, content_type="text/plain")
 
 @gzip_page
-def get_image_and_urls( request, url ):
+def get_image_and_urls(request, url):
     """Deprecated in lieu of the HAR-based 'get_dom_image'."""
-    image = base64.b64encode( generate_image( url ) )
-    urls = generate_urls( url )
-    data = [ { 'image':image, 'urls':urls } ]
-    json_string = json.dumps( data )
-    return HttpResponse( content=json_string, content_type="application/json" )
+    image = base64.b64encode(generate_image(url))
+    urls = generate_urls(url)
+    data = [{'image':image, 'urls':urls}]
+    json_string = json.dumps(data)
+    return HttpResponse(content=json_string, content_type="application/json")
 
 @gzip_page
 @csrf_exempt
-def get_dom_image( request, url ):
+def get_dom_image(request, url):
     """Tries to retrieve the HAR with rendered images, returning a 500 if timing out."""
     if request.method == "POST" and request.body:
         selectors = json.loads(request.body.decode("utf-8"))
-        har = get_har_with_image( url, selectors )
+        har = get_har_with_image(url, selectors)
     else:
-        har = get_har_with_image( url )
-    if har.startswith( "FAIL" ):
-        return HttpResponseServerError( content="%s" % har, content_type="text/plain" )
-    return HttpResponse( content=har, content_type="application/json" )
+        har = get_har_with_image(url)
+    if har.startswith("FAIL"):
+        return HttpResponseServerError(content="%s" % har, content_type="text/plain")
+    return HttpResponse(content=har, content_type="application/json")
 
