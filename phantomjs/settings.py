4f51d21e9e85c750f905a8144eb38a94b01c5c1e
Andrew Jackson <Andrew.Jackson@bl.uk>
2015-11-12 14:50:11 +0000

    Fixed up dev script location.

--- phantomjs/settings.py
+++ phantomjs/settings.py
@@ -1,7 +1,7 @@
 root = "/Users/andy/Documents/workspace/django-phantomjs/phantomjs-2.0.0-macosx"
 phantomjs = "%s/bin/phantomjs" % root
 rasterize = "./examples/rasterize.js"
-netsniff = "%s/share/phantomjs/examples/netsniff.js" % root
+netsniff = "%s/examples/netsniff.js" % root
 domimage = "./examples/netsniff-rasterize.js"
 temp = "/var/tmp"
 timeout_limit = 30
