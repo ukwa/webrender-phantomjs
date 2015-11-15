222ee53a3aaf9f124cb660621d3e5ab9763de456
Andrew Jackson <Andrew.Jackson@bl.uk>
2015-11-15 19:09:43 +0000

    Added Docker stuff for v2.

---
+++ settings-v2.py
@@ -0,0 +1,9 @@
+root = "/phantomjs"
+phantomjs = "%s/bin/phantomjs" % root
+rasterize = "%s/examples/rasterize.js" % root
+netsniff = "%s/examples/netsniff.js" % root
+domimage = "/django-phantomjs/examples/netsniff-rasterize.js"
+temp = "/var/tmp"
+timeout_limit = 30
+crop_rasterize_image = True
+
