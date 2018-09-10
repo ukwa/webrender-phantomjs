7436a13bc5633cbcfa361d84c53853cf0960f620
Andrew Jackson <Andrew.Jackson@bl.uk>
2018-09-10 11:09:54 +0100

    Attempt to remove transparency before building JPGs.

--- phantomjs/phantomjs.py
+++ phantomjs/phantomjs.py
@@ -81,6 +81,7 @@ def get_har_with_image(url, selectors=None, warcprox=WARCPROX, warc_prefix=date.
 def full_and_thumb_jpegs(large_png):
     # Load the image and drop the alpha channel:
     img = Image.open(io.BytesIO(large_png))
+    img = remove_transparency(img)
     img = img.convert("RGB")
     # Save it as a JPEG:
     out = io.BytesIO()
@@ -101,6 +102,24 @@ def full_and_thumb_jpegs(large_png):
     thumb_jpeg = out.getvalue()
 
     return full_jpeg, thumb_jpeg
+
+def remove_transparency(im, bg_colour=(255, 255, 255)):
+
+    # Only process if image has transparency (http://stackoverflow.com/a/1963146)
+    if im.mode in ('RGBA', 'LA') or (im.mode == 'P' and 'transparency' in im.info):
+
+        # Need to convert to RGBA if LA format due to a bug in PIL (http://stackoverflow.com/a/1963146)
+        alpha = im.convert('RGBA').split()[-1]
+
+        # Create a new background image of our matt color.
+        # Must be RGBA because paste requires both images have the same format
+        # (http://stackoverflow.com/a/8720632  and  http://stackoverflow.com/a/9459208)
+        bg = Image.new("RGBA", im.size, bg_colour + (255,))
+        bg.paste(im, mask=alpha)
+        return bg
+
+    else:
+        return im
 
 # HTML5: https://dev.w3.org/html5/spec-preview/image-maps.html
 # <img src="shapes.png" usemap="#shapes"
